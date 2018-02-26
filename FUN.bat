9360e999958b21254eeae5ba4dae076a25dd5cd9
XsF1re <shg1726@gmail.com>
2018-02-26 10:02:12 +0900

    Add files via upload

--- FUN.bat
+++ FUN.bat
@@ -4,11 +4,7 @@ cls
 
 :x 
 
-taskkill /f /im "ClassM_Client.exe" 
-
-taskkill /f /im "SysCtrl.exe" 
-
-taskkill /f /im "ClassM_Client_Service.exe" 
+taskkill /f /im "ClassM_Client.exe" /im "SysCtrl.exe" /im "ClassM_Client_Service.exe" 
 
 goto x: 
 
